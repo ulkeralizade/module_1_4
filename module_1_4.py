5176e6f8b469876f9711e06b21db3ea19eec6a51
ulkeralizade <ulker.100504@gmail.com>
2024-10-12 23:15:07 +0400

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string=input("введите текст:")
+length=len(my_string)
+print("количество символов в введенном тексте:",length)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ",""))
+print(my_string[0])
+print(my_string[-1])
